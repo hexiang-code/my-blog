be8535c01557e18239df40db015f1748ee6c9eaa
hexiang <hexiang@qmai.cn>
2020-01-14 22:34:00 +0800

    修改登录框

--- src/components/start-page/start-page.vue
+++ src/components/start-page/start-page.vue
@@ -23,7 +23,7 @@
             </div>
         </div>
         <div class="icon-list">
-            <img title="登录" v-if="isShowlogin" @click="loginWindow = true" class="icon-item" src="../../assets/iconfont/login.png" alt="">
+            <img title="登录" v-if="isShowlogin" @click="isShowLoginWindow = true" class="icon-item" src="../../assets/iconfont/login.png" alt="">
             <img title="注册" v-if="isShowRegister"  @click="registerWindow = true" class="icon-item" src="../../assets/iconfont/register.png" alt="">
             <img title="记事本" class="icon-item" @click="openNotepadList" src="../../assets/iconfont/txtEdit.png" alt="">
             <img title="书签" class="icon-item" @click="openBookmarkList" src="../../assets/iconfont/navigation.png" alt="">
@@ -91,7 +91,7 @@
             </div>
         </el-drawer>
         <!-- 登录弹框 -->
-        <!-- <el-dialog :visible.sync="loginWindow" title="登录" width="500px">
+        <!-- <el-dialog :visible.sync="isShowLoginWindow" title="登录" width="500px">
             <el-form label-width="80px">
                 <el-form-item label="账号">
                     <el-input type="text" v-model="userInfo.userAccount" autocomplete class="user-input" />
@@ -101,20 +101,24 @@
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="login">登录</el-button>
-                    <el-button @click="loginWindow = false">取消</el-button>
+                    <el-button @click="isShowLoginWindow = false">取消</el-button>
                 </el-form-item>
             </el-form>
         </el-dialog> -->
 
-        <div class="login-window">
-            <p class="user-accoutn">
-                <span class="login-label">ACCOUNT:</span>
+        <div class="login-window" :class="{ 'login-widow_open': isShowLoginWindow, 'login-window_closed': !isShowLoginWindow}">
+            <div class="login-input">
+                <p class="login-label">ACCOUNT:</p>
                 <input type="text" placeholder="请输入账号" />
-            </p>
-            <p class="user-password">
-                <span class="login-label">PASSWORD:</span>
+            </div>
+            <div class="login-input">
+                <p class="login-label">PASSWORD:</p>
                 <input type="password" placeholder="请输入密码" />
-            </p>
+            </div>
+            <div class="login-btn">
+                <button @click="login">confirm</button>
+                <button @click="isShowLoginWindow = false">cancel</button>
+            </div>
         </div>
 
         <!-- 注册弹框 -->
@@ -165,7 +169,7 @@ export default {
             test: '百度一下',
             // isShowlogin: true,  // 是否展示登录按钮
             isShowRegister: true,  // 是否展示注册按钮
-            loginWindow: false,  // 是否展示登录弹框
+            isShowLoginWindow: false,  // 是否展示登录弹框
             registerWindow: false,  // 是否展示注册弹框
             userInfo: {
                 userAccount: '',  // 账号
@@ -249,7 +253,7 @@ export default {
             let userInfo = this.$store.getters.getUserInfo
             if (!userInfo) {
                 this.$message.info('请登录')
-                this.loginWindow = true
+                this.isShowLoginWindow = true
                 return
             }
             let url = 'notepad/getNotepadCatalog'
@@ -351,7 +355,7 @@ export default {
                 this.$message.success('登录成功')
                 cookieServe.setCookie('token', res.token, 1)
                 this.$store.commit('setUserInfo', res.userData)
-                this.loginWindow = false
+                this.isShowLoginWindow = false
                 this.registerWindow = false
             }).catch(e => {
                 this.$message.error(e.msg)
@@ -556,13 +560,14 @@ export default {
     }
 
     .login-window {
+        visibility: hidden;
         position: fixed;
         top: 0;
         right: 0;
         bottom: 0;
         left: 0;
         width: 30%;
-        height: 300px;
+        height: 30%;
         margin: auto;
         display: flex;
         flex-direction: column;
@@ -572,48 +577,179 @@ export default {
         border-radius: 10px;
         backdrop-filter: blur(10px) brightness(110%);
         z-index: 1;
+        transform: translateY(-100%);
 
+        .login-input {
+            display: flex;
+            height: 60px;
+            align-items: center;
+        }
         .login-label {
             position: relative;
             display: inline-block;
             width: 100px;
-            margin-right: 20px;
+            margin-right: 40px;
             text-align: right;
             font-weight: 700;
             color: #1296db;
-            margin-bottom: 30px;
         }
 
         .login-label::before {
             content: '';
             position: absolute;
-            background-image: url('../../accets/')
+            width: 30px;
+            height: 30px;
+            right: -30px;
+            top: -5px;
+            background-image: url('../../assets/status-icon/leimu-icon.png');
+            background-size: 30px 30px;
         }
 
     
         input {
-            border: none;
-            background-color: transparent;
             height: 30px;
+            padding-left: 20px;
+            border: 1px solid #1296db;
             color: #1296db;
+            background-color: transparent;
+        }
+
+        .login-btn {
+            width: 100%;
+            height: 60px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .login-btn button {
+            position: relative;
+            padding: 0;
+            margin: 0;
+            width: 100px;
+            height: 40px;
+            background-color: none;
+            text-transform: uppercase;
+            border: 4px solid #1296db;
+            background-color: transparent;
+            color: #ffffff;
+            cursor: pointer;
+        }
+        
+        .login-btn button:nth-child(1) {
+            margin-right: 60px;
+            color: #1296db;
+        }
+
+        .login-btn button::after, .login-btn button::before {
+            content: '';
+            position: absolute;
+            width: 10px;
+            height: 4px;
+            background-color: #fff;
+        }
+
+        .login-btn button::after {
+            top: 32px;
+            right: 10px;
+        }
+
+        .login-btn button::before {
+            top: -4px;
+        }
+        
+        .login-btn button:hover::before {
+            animation: translateBefore 1s forwards;
+        }
+
+        .login-btn button:hover::after {
+            animation: translateAfter 1s forwards;
+        }
+
+        @keyframes translateBefore {
+            0% {
+                left: 0px;
+            }
+
+            100% {
+                left: 80px;
+                background-color: #1296db;
+            }
+        }
+
+        @keyframes translateAfter {
+            0% {
+                right: 10px;
+            }
+
+            100% {
+                right: 80px;
+                background-color: #1296db;
+            }
         }
 
         ::-webkit-input-placeholder { /* Chrome/Opera/Safari */ 
 			color: #1296db;
-            text-align: center;
 		}
 		::-moz-placeholder { /* Firefox 19+ */  
 			color: #1296db;
-            text-align: center;
 		}
 		:-ms-input-placeholder { /* IE 10+ */ 
             color: #1296db;
-            text-align: center;
 		}
 
 		:-moz-placeholder { /* Firefox 18- */ 
 		    color: #1296db;
-            text-align: center;
 		}
+    }
+
+    .login-widow_open {
+        visibility: visible;
+        animation: open-animation 1s ease-out forwards;
+
+        @keyframes open-animation {
+            0% {
+                opacity: 0;
+                transform: translateY(-100%)
+            }
+
+            30% {
+                opacity: 0.5;
+                transform: translateY(-60%)
+            }
+
+            50% {
+                opacity: 1;
+                transform: translateY(0)  
+            }
+
+            70% {
+                opacity: 1;
+                transform: translateY(-15%)  
+            }
+            
+            100% {
+                opacity: 1;
+                transform: translateY(0)
+            }
+        }
+    }
+
+    .login-window_closed {
+        visibility: visible;
+        animation: close-animation 0.5s ease-in forwards; 
+        
+        @keyframes close-animation {
+            0% {
+                transform: translateY(0);
+                opacity: 1;
+            }
+
+            100% {
+                transform: translateY(-100%);
+                opacity: 0;
+                visibility: hidden;
+            }
+        }
     }
 </style>
